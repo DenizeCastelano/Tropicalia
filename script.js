80e43b4882cddbee1b6e2c549202d78c5bf3419c
Denise Cardoso <denizecardoso@professor.educacao.sp.gov.br>
2025-07-20 21:58:11 -0300

    Aula 6 a 10

--- script.js
+++ script.js
@@ -5,9 +5,14 @@ const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
 botaoDeAcessibilidade.addEventListener('click', function (){
  botaoDeAcessibilidade.classList.toggle('rotacao-botao');
  opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+
+ const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+ botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
 })
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+
+    const alternaContraste = document.getElementById('alterna-contraste')
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
@@ -20,4 +25,13 @@ botaoDeAcessibilidade.addEventListener('click', function (){
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
     })
 
+    alternaContraste.addEventListener('click', function(){
+         document.body.classList.toggle('alto-contraste')
+ })
+
 })
+
+ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ScrollReveal().reveal('#galeria', { delay: 500 });
+ScrollReveal().reveal('#contato', { delay: 500 });
